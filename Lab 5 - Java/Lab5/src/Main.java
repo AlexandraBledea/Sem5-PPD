6a98443463f65d6f5b3973bdb36f95b50c514523
Bledea Mihaela Alexandra <bledea_alexandra@yahoo.com>
2022-12-06 22:06:14 +0200

    Added classic method with parallel technique

--- Lab 5 - Java/Lab5/src/Main.java
+++ Lab 5 - Java/Lab5/src/Main.java
@@ -1,3 +1,4 @@
+import Algorithms.ParallelClassic;
 import Algorithms.SequentialClassic;
 import Model.AlgorithmType;
 import Model.MethodType;
@@ -5,10 +6,10 @@ import Model.Polynomial;
 
 public class Main {
 
-    private static final MethodType METHOD = MethodType.SEQUENTIAL;
+    private static final MethodType METHOD = MethodType.PARALLEL;
 
     private static final AlgorithmType ALGORITHM = AlgorithmType.CLASSIC;
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Polynomial p1 = new Polynomial(3);
         System.out.println("Polynomial p1 = " + p1);
 
@@ -25,11 +26,15 @@ public class Main {
         System.out.println("Elapsed running time: " + totalTime + " s");
     }
 
-    private static void run(Polynomial polynomial1, Polynomial polynomial2){
+    private static void run(Polynomial polynomial1, Polynomial polynomial2) throws InterruptedException {
         Polynomial result = null;
         if(METHOD.equals(MethodType.SEQUENTIAL)){
             if(ALGORITHM.equals(AlgorithmType.CLASSIC)){
                 result = SequentialClassic.multiply(polynomial1, polynomial2);
+            }
+        } else if (METHOD.equals(MethodType.PARALLEL)){
+            if(ALGORITHM.equals(AlgorithmType.CLASSIC)){
+                result = ParallelClassic.multiply(polynomial1, polynomial2);
             }
         }
 
