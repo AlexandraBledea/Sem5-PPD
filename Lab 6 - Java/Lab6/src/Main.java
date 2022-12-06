c9d31bda73101de9ea2e5c57a348fd8a7b6abe43
Bledea Mihaela Alexandra <bledea_alexandra@yahoo.com>
2022-12-07 01:01:43 +0200

    Finished lab6 - java

--- Lab 6 - Java/Lab6/src/Main.java
+++ Lab 6 - Java/Lab6/src/Main.java
@@ -1,5 +1,54 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    private static final int THREAD_COUNT = 5;
+
+    private static List<List<Integer>> loadGraph(String path) throws FileNotFoundException {
+        List<List<Integer>> graph = new ArrayList<>();
+
+        try(Scanner scanner = new Scanner(new File(path))) {
+            int size = Integer.parseInt(scanner.nextLine());
+            for(int i = 0; i < size; i++){
+                graph.add(new ArrayList<>());
+            }
+            while (scanner.hasNextLine()){
+                String[] splitEdge = scanner.nextLine().split(" ");
+                graph.get(Integer.parseInt(splitEdge[0])).add(Integer.parseInt(splitEdge[1]));
+            }
+        }
+
+        return graph;
+    }
+    public static void main(String[] args) throws FileNotFoundException, InterruptedException {
+        List<List<Integer>> graph = loadGraph("graphs/g2.txt");
+
+        ExecutorService executorService = Executors.newFixedThreadPool(THREAD_COUNT);
+        AtomicBoolean foundHamiltonianCycle = new AtomicBoolean(false);
+        List<Integer> output = new ArrayList<>();
+
+        for(int i = 0; i < graph.size(); i++){
+            executorService.submit(new HamiltonianCycleSearchTask(graph, i, foundHamiltonianCycle, output));
+        }
+
+        executorService.shutdown();
+        executorService.awaitTermination(20, TimeUnit.SECONDS);
+
+        if(foundHamiltonianCycle.get()){
+            System.out.println(output);
+        }
+        else {
+            System.out.println("No hamiltonian cycles");
+        }
+
+
     }
 }
